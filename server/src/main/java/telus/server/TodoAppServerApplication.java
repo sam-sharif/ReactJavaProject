1bad5a7f7717f398c699e2750e104c3f251d3088
samir <samir_sharif@yahoo.com>
2020-07-07 12:20:09 -0400

    git started

---
+++ server/src/main/java/telus/server/TodoAppServerApplication.java
@@ -0,0 +1,13 @@
+package telus.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/telus/server/TodoAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package telus.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
